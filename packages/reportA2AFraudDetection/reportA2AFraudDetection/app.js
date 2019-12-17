6d67a159cc25e1635248e8776a8431523a236d0d
davidhorner <david.horner@alliedpayment.com>
2019-12-17 15:31:17 -0500

    changes for prod.

--- packages/reportA2AFraudDetection/reportA2AFraudDetection/app.js
+++ packages/reportA2AFraudDetection/reportA2AFraudDetection/app.js
@@ -1,6 +1,8 @@
+
+const sql = require('mssql'); // https://github.com/tediousjs/node-mssql#readme
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
-const sql = require('mssql');
+AWS.config.logger = console;
  
 let response;
 
@@ -8,93 +10,45 @@ var errHandler = function(err) {
   console.log(err);
 }
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
 exports.lambdaHandler = async (event, context) => {
-    var sqlDB="mssql://FinalSystem:gnCeUWSzqw8eFMkgUBGrr2ES4qzGQs@172.30.20.12?database=Final&encrypt=true"; 
+  var sqlDB="mssql://FinalSystem:gnCeUWSzqw8eFMkgUBGrr2ES4qzGQs@172.30.20.12?database=Final&encrypt=true"; 
+  sqlDB="mssql://ProdSystem:QAq5z%3FYdpXzLVnBFshCX%3FecCPUmLMk@172.16.6.10?database=BillPayProd&encrypt=true&Trusted_Connection=False&Min Pool Size=10&TrustServerCertificate=True"; 
     try {
-        // make sure that any items are correctly URL encoded in the connection string
-
-        var bucketName = 'reporting-a2afrauddetect';
-        var keyName = 'dude';
+        response = { 'statusCode': 403 }
+        var bucketName = process.env.DESTINATION_BUCKET;
         var AWSS3 = new AWS.S3({apiVersion: '2006-03-01'})
-        console.log("### QUERY" + AWSS3);
-        AWSS3.listBuckets(function(err, data) {
-          if (err) {
-            console.log("### Error", err);
-          } else {
-            console.log("###  Success", data.Buckets);
-          }
-       });
-      // console.log("### QUERY DONE");
-    //   var bucketPromise = AWSS3.createBucket({Bucket: bucketName}).promise();
-    //     // Call S3 to list the buckets
-    //     bucketPromise.then(
-    //       function(data) {
-    //         console.log("made bucket");
-    //       }).catch(
-    //       function(err) {
-    //         console.error("s3 bucket error:",err, err.stack);
-    //         return err;
-    //     });
   
+        var json='(null)';
+        try {
+          await sql.connect(sqlDB);
+          const result = await sql.query("SELECT p.ShardKey, u.UserName as 'Username', c.FullName as 'Customer Name', al.CreateOn as 'Create On', p.Id, p.ConfirmationNumber, p.Amount, p.PayFromAccount, al.VerifiedThrough as 'Pay From Account Verification', p.PayFromRtn, p.PayToBankAccount, p.PayToBankRoutingNumber FROM Payments p FULL OUTER JOIN Users u ON u.Id = p.UserId FULL OUTER JOIN Customers c ON c.Id = p.Customer_Id FULL OUTER JOIN A2AAuditLog al ON al.PaymentTemplateId = p.PaymentTemplateId WHERE p.AchPattern = 'A2A' and p.Amount > 250 and GETDATE() <= DATEADD(DAY, 30, al.CreateOn)")
+          json=JSON.stringify(result.recordset);
+          json=json.length>0?json:'(null)';
+        } catch(err) {
+          console.error("mssql catch:",err, err.stack);y
+        }
 
 
-            await sql.connect(sqlDB)    
-            const result = await sql.query("SELECT p.ShardKey, u.UserName as 'Username', c.FullName as 'Customer Name', al.CreateOn as 'Create On', p.Id, p.ConfirmationNumber, p.Amount, p.PayFromAccount, al.VerifiedThrough as 'Pay From Account Verification', p.PayFromRtn, p.PayToBankAccount, p.PayToBankRoutingNumber FROM Payments p FULL OUTER JOIN Users u ON u.Id = p.UserId FULL OUTER JOIN Customers c ON c.Id = p.Customer_Id FULL OUTER JOIN A2AAuditLog al ON al.PaymentTemplateId = p.PaymentTemplateId WHERE p.AchPattern = 'A2A' and p.Amount > 250 and GETDATE() <= DATEADD(DAY, 30, al.CreateOn)")
-            var json=JSON.stringify(result.recordset);
-
-
-            var now = new Date();
-            var logfile_name = 'fraud'; // + now.getFullYear() + "-"+ now.getMonth() + "-" + now.getDate() +'.json'
-            console.log("LOG: "+logfile_name);
-            var objectParams = {Bucket: bucketName, Key: logfile_name, Body: json};
-            console.log("LOG: "+JSON.stringify(objectParams));
-            
-            var objectParams2 = {Bucket: bucketName, Key: 'jack', Body: 'dave'};
-            var uploadPromise2 = AWSS3.putObject(objectParams2).promise();
-            uploadPromise2.then(
-              function(data) {
-                console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-              }).catch(
-              function(err) {
-                console.error(err, err.stack);
-                return err;
-              });
-
-              
-          var uploadPromise = AWSS3.putObject(objectParams).promise();
-          uploadPromise.then(
-            function(data) {
-              console.log("Successfully uploaded data to " + bucketName + "/" + logfile_name);
-            }).catch(
-            function(err) {
-              console.error(err, err.stack);
-              return err;
-            }).then(function(data) {
-              console.log(data);
-          }, errHandler);
-            console.log("LOG: "+logfile_name);
-            response = {
-              'statusCode': 200,
-              'body': json
-            }
-
-  
-
-            //console.log(response.body);
+        var now = new Date();
+        var logfile_name = now.getFullYear()+'/'+(now.getMonth()+1)+'/fraud-' + now.getFullYear() + "-"+ (now.getMonth()+1) + "-" + now.getDate() +'.json'
+        var objectParams = {Bucket: bucketName, Key: logfile_name, Body: json};
+        console.log("LOG: "+JSON.stringify(objectParams));
+        var uploadPromise = AWSS3.putObject(objectParams).promise();
+        uploadPromise.then(
+          function(data) {
+            console.log("Successfully uploaded data to " + bucketName + "/" + logfile_name);
+            response = { 'statusCode': 200 }
+          }).catch(
+          function(err) {
+            console.log("upload failed:",JSON.stringify(err));
+            console.error("upload failed:",JSON.stringify(err));
+            return err;
+          }).then(function(data) {
+            console.log(data);
+        }, errHandler);
+        await uploadPromise;
       } catch (err) {
-       console.log("s3 bucket error:",err);
+       console.log("s3 catch(",bucketName,"):",err);
        return err;
      }    
     return response
